60d41854355b3b86d91e362d10766f1bdb22759a
Vasyl <okvasiliy@yahoo.com>
2018-08-27 17:40:52 -0500

    added navigation

---
+++ ProjectLuna/Assets/Scripts/enemy/enemy_navigtaion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy_navigtaion : MonoBehaviour {
+
+    public Transform goal;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
