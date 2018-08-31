9a6f1177fd636e94402d1395d7ba1064b93789bf
Vasyl <okvasiliy@yahoo.com>
2018-08-31 12:16:29 -0500

    revamped player movement & animations

--- ProjectLuna/Assets/Scripts/player/player_movement.cs
+++ ProjectLuna/Assets/Scripts/player/player_movement.cs
@@ -8,26 +8,29 @@ public class player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        anim.SetBool("movekeydown", false);
         if (Input.GetKey(KeyCode.W))
         {
             gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * _forcemod * Time.deltaTime * 100, ForceMode.Force);
+            anim.SetBool("movekeydown", true);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             gameObject.GetComponent<Rigidbody>().AddForce(Vector3.back * _forcemod * Time.deltaTime * 100, ForceMode.Force);
+            anim.SetBool("movekeydown", true);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left * _forcemod * Time.deltaTime * 100, ForceMode.Force);
+            anim.SetBool("movekeydown", true);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * _forcemod * Time.deltaTime * 100, ForceMode.Force);
+            anim.SetBool("movekeydown", true);
         }
-
-        anim.SetFloat("Velocity", gameObject.GetComponent<Rigidbody>().velocity.magnitude);
     }
 }
