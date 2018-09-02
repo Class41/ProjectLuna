d341f248e33886d412a73d331d7240f0f29a1159
Vasyl <okvasiliy@yahoo.com>
2018-09-01 20:55:43 -0500

    commiting asset database before switching to moonlight

---
+++ ProjectLuna/Assets/Scripts/lighting/light_followplayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light_followplayer : MonoBehaviour
+{
+
+    public Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(player.position.x, Mathf.Lerp(gameObject.transform.position.y ,(player.position.y + 4), .1f), player.position.z - 2);
+    }
+}
