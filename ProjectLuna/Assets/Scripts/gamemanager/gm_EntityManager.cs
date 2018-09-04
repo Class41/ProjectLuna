d493868cf49cda0faa5616cd42d255480817ef94
Vasyl Onufriyev <projectclass41@gmail.com>
2018-09-03 22:54:33 -0500

    Added screen-to-world placeable capability

---
+++ ProjectLuna/Assets/Scripts/gamemanager/gm_EntityManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class gm_EntityManager : MonoBehaviour
+{
+
+    public List<GameObject> activeEntities = new List<GameObject>();
+    public List<GameObject> availableEntitiesList = new List<GameObject>();
+
+    public GameObject _selectedEntity,
+                      _player;
+
+    public Camera _currentCamera;
+
+    public Vector3 _lastSelectedWorldPoint;
+
+    void OnGUI()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _selectedEntity = availableEntitiesList[0]; //FOR TESTING PURPOSES ONLY. REMOVE THIS LINE LATER!!!!!!!!!!!!!!!!!!!!!!!@@@@@@@
+
+            if (_selectedEntity != null)
+            {
+                RaycastHit hit;
+                Ray ray = _currentCamera.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    _lastSelectedWorldPoint = hit.point;
+                }
+
+                spawnEntity(_selectedEntity, _player);
+            }
+        }
+    }
+
+    public void spawnEntity(GameObject ent, GameObject creator = null)
+    {
+        GameObject spawnedEnt = Instantiate(ent, _lastSelectedWorldPoint, ent.transform.rotation) as GameObject;
+
+        if (creator != null)
+            spawnedEnt.GetComponent<world_entitybase>()._creator = creator.transform;
+
+        _selectedEntity = null;
+
+        activeEntities.Add(spawnedEnt);
+    }
+}
---
+++ ProjectLuna/Assets/Scripts/worldentity/world_entitybase.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class world_entitybase : MonoBehaviour
+{
+    public float _expiration,
+                 _durability,
+                 _distanceMaxFromCreator;
+
+    public Vector3 _position;
+
+    public Transform _creator;
+
+    public List<string> acceptableSpawnTags = new List<string>();
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (_expiration < Time.timeSinceLevelLoad || _durability <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
