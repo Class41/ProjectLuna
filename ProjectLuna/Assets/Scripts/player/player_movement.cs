b4ceec7a3d32532522f4159f81c877764c3a5bda
Vasyl Onufriyev <projectclass41@gmail.com>
2018-10-19 19:07:12 -0500

    Fixed player rotation while moving

--- ProjectLuna/Assets/Scripts/player/player_movement.cs
+++ ProjectLuna/Assets/Scripts/player/player_movement.cs
@@ -9,12 +9,21 @@ public class player_movement : MonoBehaviour
     public float _barrierVisibility = 0,
                  _barrierDisappearTime = 0;
 
+    public Rigidbody _plyRigidbody;
+    public Transform _plyTranform;
+
     private void OnCollisionStay(Collision collision)
     {
         if(collision.transform.CompareTag("invisbarrierwall"))
         {
             _barrierVisibility = .25f;
         }
+    }
+
+    private void Start()
+    {
+        _plyRigidbody = gameObject.GetComponent<Rigidbody>();
+        _plyTranform = gameObject.GetComponent<Transform>();
     }
 
     void Update()
@@ -29,26 +38,29 @@ public class player_movement : MonoBehaviour
         _anim.SetBool("movekeydown", false);
         if (Input.GetKey(KeyCode.W))
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * _forcemod * Time.deltaTime * 100, ForceMode.Force);
+            _plyRigidbody.AddForce(Vector3.forward * _forcemod * Time.deltaTime * 100, ForceMode.Force);          
             _anim.SetBool("movekeydown", true);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.back * _forcemod * Time.deltaTime * 100, ForceMode.Force);
+            _plyRigidbody.AddForce(Vector3.back * _forcemod * Time.deltaTime * 100, ForceMode.Force);
             _anim.SetBool("movekeydown", true);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left * _forcemod * Time.deltaTime * 100, ForceMode.Force);
+            _plyRigidbody.AddForce(Vector3.left * _forcemod * Time.deltaTime * 100, ForceMode.Force);
             _anim.SetBool("movekeydown", true);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * _forcemod * Time.deltaTime * 100, ForceMode.Force);
+            _plyRigidbody.AddForce(Vector3.right * _forcemod * Time.deltaTime * 100, ForceMode.Force);
             _anim.SetBool("movekeydown", true);
         }
+
+        if (_plyRigidbody.velocity.magnitude > 1)
+            _plyTranform.forward = new Vector3(_plyRigidbody.velocity.normalized.x, 0, _plyRigidbody.velocity.normalized.z);
     }
 }
