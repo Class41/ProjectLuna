20df79b6548ae4bdcd2bf30825dc2d64559a7a40
Vasyl Onufriyev <projectclass41@gmail.com>
2018-11-30 11:19:01 -0600

    Added menu music

--- ProjectLuna/Assets/Scripts/mainmenu/ui_funccontroller.cs
+++ ProjectLuna/Assets/Scripts/mainmenu/ui_funccontroller.cs
@@ -9,10 +9,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ui_funccontroller : MonoBehaviour
 {
     public Animator _menuAnim;
+    public Text _tutText;
 
     public void Start()
     {
@@ -29,9 +31,21 @@ public class ui_funccontroller : MonoBehaviour
         Application.Quit();
     }
 
+    public void SwapTutText()
+    {
+        if (_tutText.text == "Tutorial")
+            _tutText.text = "Back";
+        else
+            _tutText.text = "Tutorial";
+    }
+
+    /// <summary>
+    /// <para>Called by UI to toggle tut mode</para>
+    /// </summary>
     public void TutToggle()
     {
         _menuAnim.SetBool("TutorialMode", !_menuAnim.GetBool("TutorialMode"));
+        Invoke("SwapTutText", .55f);
     }
 
 }
