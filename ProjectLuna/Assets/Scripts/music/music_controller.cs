e61e061c957f94af070424f0e771aa8377fc7d5c
Vasyl Onufriyev <projectclass41@gmail.com>
2018-12-02 22:46:45 -0600

    added sounds to the main game (theme/ply health)

---
+++ ProjectLuna/Assets/Scripts/music/music_controller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class music_controller : MonoBehaviour
+{
+    public AudioSource _musicPrimary;
+    public Animator _shopAnim;
+
+    public AudioSource _healthSounds;
+    public player_health _playerStats;
+
+
+
+    void Start()
+    {
+        _musicPrimary.time = Random.Range(0.0f, _musicPrimary.clip.length);
+    }
+
+    void Update()
+    {
+        bool shopstat = (_shopAnim.GetBool("shopopen")) ? true : false;
+        if (_musicPrimary.volume < 1.0f && !shopstat)
+            _musicPrimary.volume = Mathf.Lerp(_musicPrimary.volume, 1.0f, .2f * Time.deltaTime);
+        else if (shopstat)
+            _musicPrimary.volume = .3f;
+
+        if (_playerStats._health <= 0)
+        {
+            _musicPrimary.volume = .1f;
+            _healthSounds.volume = .3f;
+        }
+
+        _healthSounds.volume = 1.0f - (_playerStats._health / _playerStats._maxHealth);
+    }
+}
