7b058292369a983eb540aba584c1918b8e47ebbe
Vasyl Onufriyev <projectclass41@gmail.com>
2018-12-05 11:32:47 -0600

    Added walk SFX / Built

---
+++ ProjectLuna/Assets/Scripts/sound/effects/walking/sound_entwalking.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound_entwalking : MonoBehaviour
+{
+    public List<AudioClip> _walkClips = new List<AudioClip>();
+    public AudioSource _walkPlayer;
+
+    public void PlaySoundStep()
+    {
+        _walkPlayer.clip = _walkClips[Random.Range(0, _walkClips.Count - 1)];
+        _walkPlayer.Play();
+    }
+}
