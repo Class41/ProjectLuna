bff568ce4e325386bd41bd60caabceeee03fb088
Vasyl Onufriyev <projectclass41@gmail.com>
2018-12-07 00:36:34 -0600

    added sound to all enemies

---
+++ ProjectLuna/Assets/Scripts/sound/effects/attack/enemy/sound_enemysound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound_enemysound : MonoBehaviour
+{
+    public AudioSource _attackSource;
+
+    public void PlayAttackSound()
+    {
+        _attackSource.Play();
+    }
+}
