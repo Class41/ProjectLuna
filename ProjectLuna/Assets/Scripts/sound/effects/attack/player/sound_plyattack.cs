c3200c6455a8b65e950cc6f0ef0573575f06b1c3
Vasyl Onufriyev <projectclass41@gmail.com>
2018-12-07 00:05:53 -0600

    Pushing player attack sounds before something else goes wrong

---
+++ ProjectLuna/Assets/Scripts/sound/effects/attack/player/sound_plyattack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound_plyattack : MonoBehaviour
+{
+    public AudioClip[] _plyAttackSounds = new AudioClip[3];
+    public AudioSource _plyAttackSoundSource;
+
+    public void PlayAttackSound(int index)
+    {
+        _plyAttackSoundSource.clip = _plyAttackSounds[index];
+        _plyAttackSoundSource.Play();
+    }
+}
